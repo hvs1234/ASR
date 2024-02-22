5b3fc3d46c0bdf7076cf07a5624b342732553791
Harshvardhan Sharma <102682161+hvs1234@users.noreply.github.com>
2024-02-22 08:52:20 +0530

    Update script.js

--- script.js
+++ script.js
@@ -32,3 +32,142 @@ document.querySelector('button').addEventListener('click',()=>{
     speech.text = document.querySelector('textarea').value;
     window.speechSynthesis.speak(speech);
 });
+
+/*
+// Scroll To Top
+
+const scroll = document.querySelector('.scroll-top');
+scroll.addEventListener('click',() => {
+    window.scroll({
+        top: 0,
+        behavior: "smooth",
+    });
+});
+
+// Text To Speech
+
+let speech = new SpeechSynthesisUtterance();
+let voices = [];
+
+let voiceSelect = document.querySelector('select')
+
+window.speechSynthesis.onvoiceschanged = ()=> {
+    voices = window.speechSynthesis.getVoices();
+    speech.voice = voices[0];
+
+    voices.forEach((voice,i)=>{
+        voiceSelect.options[i] = new Option(voice.name,i);
+    })
+};
+
+voiceSelect.addEventListener('change',()=>{
+    speech.voice = voices[voiceSelect.value]
+})
+
+document.querySelector('button').addEventListener('click',()=>{
+    speech.text = document.querySelector('textarea').value;
+    window.speechSynthesis.speak(speech);
+});
+// Scroll To Top
+
+const scroll = document.querySelector('.scroll-top');
+scroll.addEventListener('click',() => {
+    window.scroll({
+        top: 0,
+        behavior: "smooth",
+    });
+});
+
+// Text To Speech
+
+let speech = new SpeechSynthesisUtterance();
+let voices = [];
+
+let voiceSelect = document.querySelector('select')
+
+window.speechSynthesis.onvoiceschanged = ()=> {
+    voices = window.speechSynthesis.getVoices();
+    speech.voice = voices[0];
+
+    voices.forEach((voice,i)=>{
+        voiceSelect.options[i] = new Option(voice.name,i);
+    })
+};
+
+voiceSelect.addEventListener('change',()=>{
+    speech.voice = voices[voiceSelect.value]
+})
+
+document.querySelector('button').addEventListener('click',()=>{
+    speech.text = document.querySelector('textarea').value;
+    window.speechSynthesis.speak(speech);
+});
+// Scroll To Top
+
+const scroll = document.querySelector('.scroll-top');
+scroll.addEventListener('click',() => {
+    window.scroll({
+        top: 0,
+        behavior: "smooth",
+    });
+});
+
+// Text To Speech
+
+let speech = new SpeechSynthesisUtterance();
+let voices = [];
+
+let voiceSelect = document.querySelector('select')
+
+window.speechSynthesis.onvoiceschanged = ()=> {
+    voices = window.speechSynthesis.getVoices();
+    speech.voice = voices[0];
+
+    voices.forEach((voice,i)=>{
+        voiceSelect.options[i] = new Option(voice.name,i);
+    })
+};
+
+voiceSelect.addEventListener('change',()=>{
+    speech.voice = voices[voiceSelect.value]
+})
+
+document.querySelector('button').addEventListener('click',()=>{
+    speech.text = document.querySelector('textarea').value;
+    window.speechSynthesis.speak(speech);
+});
+// Scroll To Top
+
+const scroll = document.querySelector('.scroll-top');
+scroll.addEventListener('click',() => {
+    window.scroll({
+        top: 0,
+        behavior: "smooth",
+    });
+});
+
+// Text To Speech
+
+let speech = new SpeechSynthesisUtterance();
+let voices = [];
+
+let voiceSelect = document.querySelector('select')
+
+window.speechSynthesis.onvoiceschanged = ()=> {
+    voices = window.speechSynthesis.getVoices();
+    speech.voice = voices[0];
+
+    voices.forEach((voice,i)=>{
+        voiceSelect.options[i] = new Option(voice.name,i);
+    })
+};
+
+voiceSelect.addEventListener('change',()=>{
+    speech.voice = voices[voiceSelect.value]
+})
+
+document.querySelector('button').addEventListener('click',()=>{
+    speech.text = document.querySelector('textarea').value;
+    window.speechSynthesis.speak(speech);
+});
+*/
